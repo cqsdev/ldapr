cc2211e9d5da7dfc1044e2ab9576e5a498f53657
Will Gray <w.gray@vumc.org>
2023-10-09 19:00:50 -0500

    Use cat instead of paste for printing

--- R/print.R
+++ R/print.R
@@ -2,13 +2,11 @@
 ldap_print <- function(
   private
 ){
-  sep <- "\n  "
-  x <- "<LDAP connection>"
-  x <- paste(x, paste0("URI: ", private$uri), sep = sep)
-  x <- paste(x, paste0("Authenticated: ", private$authenticated), sep = sep)
-  if(private$authenticated){
-    x <- paste(x, paste0("Authenticated user: ", private$authenticated_user), sep = sep)
-    x <- paste(x, paste0("Authenticated until: ", private$authenticated_until), sep = sep)
+  cat("<LDAP connection>\n")
+  cat(paste0("URI: ", private$uri), "\n")
+  cat(paste0("Authenticated: ", private$authenticated), "\n")
+  if (private$authenticated) {
+    cat(paste0("Authenticated user: ", private$authenticated_user), "\n")
+    cat(paste0("Authenticated until: ", private$authenticated_until), "\n")
   }
-  cat(x)
 }
